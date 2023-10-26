ff0cc3905035246a7f5229d0acecc65a19eec486
Rina <kevalanirina1511@gmail.com>
2023-10-26 18:59:37 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './MyComponents/todos/todos.component';
 import { FormsModule } from '@angular/forms';
-import { LoginComponent } from './MyComponents/login/login.component';
-import { SignUpComponent } from './MyComponents/sign-up/sign-up.component';
 import { RouterModule, Routes,Router } from '@angular/router';
 
 const appRoute : Routes=[
-  {path : 'home', component: AppComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'signup', component: SignUpComponent}
+  {path : 'home', component: AppComponent}
 ]
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
-    LoginComponent,
-    SignUpComponent,
-
   ],
   imports: [
     BrowserModule,
